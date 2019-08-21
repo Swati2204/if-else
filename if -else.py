d8e3d966ba7c8bef2fb07b98b3459375e77f1cd4
Swati <54058413+Swati2204@users.noreply.github.com>
2019-08-21 20:49:59 +0530

    Add files via upload

---
+++ if -else.py
@@ -0,0 +1,34 @@
+ ''' syntasx of if statment:
+        if expression :
+           statment(s)
+         syntax of if-else
+         if exp :
+            statment(s)
+         else :
+            statment(s)
+      syntax of elif statment
+      if exp1 :
+          statment(s)
+      elif exp2:
+          statment(s)
+      elif exp3:
+          statment(s)
+      else:
+          statment(s)
+      '''
+    # program to find which number is greater.
+    
+    a = 33
+    b = 50
+    if b > a:
+    print("b is greater than a")
+
+# program to check which number is greater using elif.
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
